c806e059acb152913d3f7ffc2c26ff117b3ed363
wins <wins>
2023-09-26 15:11:17 +0300

    the and

--- HelloWordApp/ViewController.swift
+++ HelloWordApp/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var helloWorldLable: UILabel!
+    @IBOutlet var startButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        helloWorldLable.isHidden = true
+        startButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func startButtonPrest() {
+        if helloWorldLable.isHidden {
+        helloWorldLable.isHidden = false
+        startButton.setTitle("Clear Text", for: .normal)
+        } else {
+            helloWorldLable.isHidden = true
+            startButton.setTitle("Show Text", for: .normal)
+        }
+        
+    }
+    
 }
 
